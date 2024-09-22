a35e00c70eb7398ce9b8250e0eccfe3238f83d3b
John Prabu A <johnprabu0702@gmail.com>
2024-09-23 02:46:34 +0530

    Snr Birthday Special website V1

---
+++ temp.js
@@ -0,0 +1,26 @@
+const music_list = [
+  {
+    name: "Heeriye Heeriye",
+    artist: "Arijit Singh and Jasleen Royal",
+    music: "./audio/song_1.mpeg",
+    img: "./images/heeriye_cover.jpeg",
+  },
+  {
+    name: "Channa Mereya",
+    artist: "Arijit Singh",
+    music: "./audio/song_2.mpeg",
+    img: "./images/channa_cover.jpeg",
+  },
+  {
+    name: "Kun Faya Kun",
+    artist: "A.R. Rahman",
+    music: "./audio/song_4.mpeg",
+    img: "./images/kun_cover.jpeg",
+  },
+  {
+    name: "Daylight",
+    artist: "Taylor Swift",
+    music: "./audio/song_3.mpeg",
+    img: "./images/daylight_cover.jpeg",
+  },
+];
